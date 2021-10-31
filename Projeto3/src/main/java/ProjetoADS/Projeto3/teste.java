cd94a4953635b7af767ca6b38ec250c860b9d5ee
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-31 22:50:28 +0000

    xxx

--- Projeto3/src/main/java/ProjetoADS/Projeto3/teste.java
+++ Projeto3/src/main/java/ProjetoADS/Projeto3/teste.java
@@ -28,56 +28,55 @@ public class teste {
 			stmt = conn.createStatement();
 			results = stmt.executeQuery(sql_select);
 
-			List<Sala> studentsList = new ArrayList<Sala>();
+			List<Sala> salasList = new ArrayList<Sala>();
 
 			while (results.next()) {
 
-				Sala stdObject = new Sala();
+				Sala aux_sala = new Sala();
 				System.out.println(results.getString("Nome Sala"));
-				stdObject.setEdificio(results.getString("Edifício"));
-				stdObject.setNomeSala(results.getString("Nome Sala"));
-				stdObject.setCapacidadeNormal(results.getInt("Capacidade Normal"));
-				stdObject.setCapacidadeExame(results.getInt("Capacidade Exame"));				
-				stdObject.setN_caracteristicas(results.getInt("Nº características"));	
-				stdObject.setAnfiteatroaulas(results.getString("Anfiteatro aulas").isBlank());
-				stdObject.setApoiotecnicoeventos(results.getString("Apoio técnico eventos").isBlank());
-				stdObject.setArq1(results.getString("Arq 1").isBlank());
-				stdObject.setArq2(results.getString("Arq 2").isBlank());
-				stdObject.setArq3(results.getString("Arq 3").isBlank());
-				stdObject.setArq4(results.getString("Arq 4").isBlank());
-				stdObject.setArq5(results.getString("Arq 5").isBlank());
-				stdObject.setArq6(results.getString("Arq 6").isBlank());
-				stdObject.setArq9(results.getString("Arq 9").isBlank());
-				stdObject.setBYOD(results.getString("BYOD (Bring Your Own Device)").isBlank());
-				stdObject.setFocusGroup(results.getString("Focus Group").isBlank());
-				stdObject.setHorariosalavisivelportalpublico(results.getString("Horário sala visível portal público").isBlank());
-				stdObject.setLaboratoriodeArquitecturadeComputadoresI(results.getString("Laboratório de Arquitectura de Computadores I").isBlank());
-				stdObject.setLaboratoriodeArquitecturadeComputadoresII(results.getString("Laboratório de Arquitectura de Computadores II").isBlank());
-				stdObject.setLaboratoriodeBasesdeEngenharia(results.getString("Laboratório de Bases de Engenharia").isBlank());
-				stdObject.setLaboratoriodeElectronica(results.getString("Laboratório de Electrónica").isBlank());
-				stdObject.setLaboratoriodeInformatica(results.getString("Laboratório de Informática").isBlank());
-				stdObject.setLaboratoriodeJornalismo(results.getString("Laboratório de Jornalismo").isBlank());
-				stdObject.setLaboratoriodeRedesdeComputadoresI(results.getString("Laboratório de Redes de Computadores I").isBlank());
-				stdObject.setLaboratoriodeRedesdeComputadoresII(results.getString("Laboratório de Redes de Computadores II").isBlank());
-				stdObject.setLaboratoriodeTelecomunicacoes(results.getString("Laboratório de Telecomunicações").isBlank());
-				stdObject.setSalaAulasMestrado(results.getString("Sala Aulas Mestrado").isBlank());
-				stdObject.setSalaAulasMestradoPlus(results.getString("Sala Aulas Mestrado Plus").isBlank());
-				stdObject.setSalaNEE(results.getString("Sala NEE").isBlank());
-				stdObject.setSalaProvas(results.getString("Sala Provas").isBlank());
-				stdObject.setSalaReuniao(results.getString("Sala Reunião").isBlank());
-				stdObject.setSaladeArquitectura(results.getString("Sala de Arquitectura").isBlank());
-				stdObject.setSaladeAulasnormal(results.getString("Sala de Aulas normal").isBlank());
-				stdObject.setVideoconferencia(results.getString("videoconferencia").isBlank());
-				stdObject.setAtrio(results.getString("Átrio").isBlank());
+				aux_sala.setEdificio(results.getString("Edifício"));
+				aux_sala.setNomeSala(results.getString("Nome Sala"));
+				aux_sala.setCapacidadeNormal(results.getInt("Capacidade Normal"));
+				aux_sala.setCapacidadeExame(results.getInt("Capacidade Exame"));				
+				aux_sala.setN_caracteristicas(results.getInt("Nº características"));	
+				aux_sala.setAnfiteatroaulas(results.getString("Anfiteatro aulas").isBlank());
+				aux_sala.setApoiotecnicoeventos(results.getString("Apoio técnico eventos").isBlank());
+				aux_sala.setArq1(results.getString("Arq 1").isBlank());
+				aux_sala.setArq2(results.getString("Arq 2").isBlank());
+				aux_sala.setArq3(results.getString("Arq 3").isBlank());
+				aux_sala.setArq4(results.getString("Arq 4").isBlank());
+				aux_sala.setArq5(results.getString("Arq 5").isBlank());
+				aux_sala.setArq6(results.getString("Arq 6").isBlank());
+				aux_sala.setArq9(results.getString("Arq 9").isBlank());
+				aux_sala.setBYOD(results.getString("BYOD (Bring Your Own Device)").isBlank());
+				aux_sala.setFocusGroup(results.getString("Focus Group").isBlank());
+				aux_sala.setHorariosalavisivelportalpublico(results.getString("Horário sala visível portal público").isBlank());
+				aux_sala.setLaboratoriodeArquitecturadeComputadoresI(results.getString("Laboratório de Arquitectura de Computadores I").isBlank());
+				aux_sala.setLaboratoriodeArquitecturadeComputadoresII(results.getString("Laboratório de Arquitectura de Computadores II").isBlank());
+				aux_sala.setLaboratoriodeBasesdeEngenharia(results.getString("Laboratório de Bases de Engenharia").isBlank());
+				aux_sala.setLaboratoriodeElectronica(results.getString("Laboratório de Electrónica").isBlank());
+				aux_sala.setLaboratoriodeInformatica(results.getString("Laboratório de Informática").isBlank());
+				aux_sala.setLaboratoriodeJornalismo(results.getString("Laboratório de Jornalismo").isBlank());
+				aux_sala.setLaboratoriodeRedesdeComputadoresI(results.getString("Laboratório de Redes de Computadores I").isBlank());
+				aux_sala.setLaboratoriodeRedesdeComputadoresII(results.getString("Laboratório de Redes de Computadores II").isBlank());
+				aux_sala.setLaboratoriodeTelecomunicacoes(results.getString("Laboratório de Telecomunicações").isBlank());
+				aux_sala.setSalaAulasMestrado(results.getString("Sala Aulas Mestrado").isBlank());
+				aux_sala.setSalaAulasMestradoPlus(results.getString("Sala Aulas Mestrado Plus").isBlank());
+				aux_sala.setSalaNEE(results.getString("Sala NEE").isBlank());
+				aux_sala.setSalaProvas(results.getString("Sala Provas").isBlank());
+				aux_sala.setSalaReuniao(results.getString("Sala Reunião").isBlank());
+				aux_sala.setSaladeArquitectura(results.getString("Sala de Arquitectura").isBlank());
+				aux_sala.setSaladeAulasnormal(results.getString("Sala de Aulas normal").isBlank());
+				aux_sala.setVideoconferencia(results.getString("videoconferencia").isBlank());
+				aux_sala.setAtrio(results.getString("Átrio").isBlank());
 				
 				
-				studentsList.add(stdObject);
+				salasList.add(aux_sala);
 			}
 
 			ObjectMapper mapper = new ObjectMapper();
-			String JSONOutput = mapper.writeValueAsString(studentsList);
+			String JSONOutput = mapper.writeValueAsString(salasList);
 			System.out.println(JSONOutput);
-			System.out.println(studentsList );
 
 		} catch (SQLException e) {
 			e.printStackTrace();
