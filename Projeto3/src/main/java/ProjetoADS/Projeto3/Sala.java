fe7d1b46e5985a8ecf9d35f16a6ba968ac07f8bc
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-31 21:05:25 +0000

    asfrghjkl

---
+++ Projeto3/src/main/java/ProjetoADS/Projeto3/Sala.java
@@ -0,0 +1,321 @@
+package ProjetoADS.Projeto3;
+
+public class Sala {
+
+	private String Edificio;
+	private String NomeSala;
+	private int CapacidadeNormal;
+	private int CapacidadeExame;
+	private int n_caracteristicas;
+	private boolean Anfiteatroaulas;
+	private boolean Apoiotecnicoeventos;
+	private boolean Arq1;
+	private boolean Arq2;
+	private boolean Arq3;
+	private boolean Arq4;
+	private boolean Arq5;
+	private boolean Arq6;
+	private boolean Arq9;
+	private boolean BYOD;
+	private boolean FocusGroup;
+	private boolean Horariosalavisivelportalpublico;
+	private boolean LaboratoriodeArquitecturadeComputadoresI;
+	private boolean LaboratoriodeArquitecturadeComputadoresII;
+	private boolean LaboratoriodeBasesdeEngenharia;
+	private boolean LaboratoriodeElectronica;
+	private boolean LaboratoriodeInformatica;
+	private boolean LaboratoriodeJornalismo;
+	private boolean LaboratoriodeRedesdeComputadoresI;
+	private boolean LaboratoriodeRedesdeComputadoresII;
+	private boolean LaboratoriodeTelecomunicacoes;
+	private boolean SalaAulasMestrado;
+	private boolean SalaAulasMestradoPlus;
+	private boolean SalaNEE;
+	private boolean SalaProvas;
+	private boolean SalaReuniao;
+	private boolean SaladeArquitectura;
+	private boolean SaladeAulasnormal;
+	private boolean videoconferencia;
+	private boolean Atrio;
+
+	public String getEdificio() {
+		return Edificio;
+	}
+
+	public void setEdificio(String edificio) {
+		Edificio = edificio;
+	}
+
+	public String getNomeSala() {
+		return NomeSala;
+	}
+
+	public void setNomeSala(String nomeSala) {
+		NomeSala = nomeSala;
+	}
+
+	public int getCapacidadeNormal() {
+		return CapacidadeNormal;
+	}
+
+	public void setCapacidadeNormal(int capacidadeNormal) {
+		CapacidadeNormal = capacidadeNormal;
+	}
+
+	public int getCapacidadeExame() {
+		return CapacidadeExame;
+	}
+
+	public void setCapacidadeExame(int capacidadeExame) {
+		CapacidadeExame = capacidadeExame;
+	}
+
+	public int getN_caracteristicas() {
+		return n_caracteristicas;
+	}
+
+	public void setN_caracteristicas(int n_caracteristicas) {
+		this.n_caracteristicas = n_caracteristicas;
+	}
+
+	public boolean isAnfiteatroaulas() {
+		return Anfiteatroaulas;
+	}
+
+	public void setAnfiteatroaulas(boolean anfiteatroaulas) {
+		Anfiteatroaulas = anfiteatroaulas;
+	}
+
+	public boolean isApoiotecnicoeventos() {
+		return Apoiotecnicoeventos;
+	}
+
+	public void setApoiotecnicoeventos(boolean apoiotecnicoeventos) {
+		Apoiotecnicoeventos = apoiotecnicoeventos;
+	}
+
+	public boolean isArq1() {
+		return Arq1;
+	}
+
+	public void setArq1(boolean arq1) {
+		Arq1 = arq1;
+	}
+
+	public boolean isArq2() {
+		return Arq2;
+	}
+
+	public void setArq2(boolean arq2) {
+		Arq2 = arq2;
+	}
+
+	public boolean isArq3() {
+		return Arq3;
+	}
+
+	public void setArq3(boolean arq3) {
+		Arq3 = arq3;
+	}
+
+	public boolean isArq4() {
+		return Arq4;
+	}
+
+	public void setArq4(boolean arq4) {
+		Arq4 = arq4;
+	}
+
+	public boolean isArq5() {
+		return Arq5;
+	}
+
+	public void setArq5(boolean arq5) {
+		Arq5 = arq5;
+	}
+
+	public boolean isArq6() {
+		return Arq6;
+	}
+
+	public void setArq6(boolean arq6) {
+		Arq6 = arq6;
+	}
+
+	public boolean isArq9() {
+		return Arq9;
+	}
+
+	public void setArq9(boolean arq9) {
+		Arq9 = arq9;
+	}
+
+	public boolean isBYOD() {
+		return BYOD;
+	}
+
+	public void setBYOD(boolean bYOD) {
+		BYOD = bYOD;
+	}
+
+	public boolean isFocusGroup() {
+		return FocusGroup;
+	}
+
+	public void setFocusGroup(boolean focusGroup) {
+		FocusGroup = focusGroup;
+	}
+
+	public boolean isHorariosalavisivelportalpublico() {
+		return Horariosalavisivelportalpublico;
+	}
+
+	public void setHorariosalavisivelportalpublico(boolean horariosalavisivelportalpublico) {
+		Horariosalavisivelportalpublico = horariosalavisivelportalpublico;
+	}
+
+	public boolean isLaboratoriodeArquitecturadeComputadoresI() {
+		return LaboratoriodeArquitecturadeComputadoresI;
+	}
+
+	public void setLaboratoriodeArquitecturadeComputadoresI(boolean laboratoriodeArquitecturadeComputadoresI) {
+		LaboratoriodeArquitecturadeComputadoresI = laboratoriodeArquitecturadeComputadoresI;
+	}
+
+	public boolean isLaboratoriodeArquitecturadeComputadoresII() {
+		return LaboratoriodeArquitecturadeComputadoresII;
+	}
+
+	public void setLaboratoriodeArquitecturadeComputadoresII(boolean laboratoriodeArquitecturadeComputadoresII) {
+		LaboratoriodeArquitecturadeComputadoresII = laboratoriodeArquitecturadeComputadoresII;
+	}
+
+	public boolean isLaboratoriodeBasesdeEngenharia() {
+		return LaboratoriodeBasesdeEngenharia;
+	}
+
+	public void setLaboratoriodeBasesdeEngenharia(boolean laboratoriodeBasesdeEngenharia) {
+		LaboratoriodeBasesdeEngenharia = laboratoriodeBasesdeEngenharia;
+	}
+
+	public boolean isLaboratoriodeElectronica() {
+		return LaboratoriodeElectronica;
+	}
+
+	public void setLaboratoriodeElectronica(boolean laboratoriodeElectronica) {
+		LaboratoriodeElectronica = laboratoriodeElectronica;
+	}
+
+	public boolean isLaboratoriodeInformatica() {
+		return LaboratoriodeInformatica;
+	}
+
+	public void setLaboratoriodeInformatica(boolean laboratoriodeInformatica) {
+		LaboratoriodeInformatica = laboratoriodeInformatica;
+	}
+
+	public boolean isLaboratoriodeJornalismo() {
+		return LaboratoriodeJornalismo;
+	}
+
+	public void setLaboratoriodeJornalismo(boolean laboratoriodeJornalismo) {
+		LaboratoriodeJornalismo = laboratoriodeJornalismo;
+	}
+
+	public boolean isLaboratoriodeRedesdeComputadoresI() {
+		return LaboratoriodeRedesdeComputadoresI;
+	}
+
+	public void setLaboratoriodeRedesdeComputadoresI(boolean laboratoriodeRedesdeComputadoresI) {
+		LaboratoriodeRedesdeComputadoresI = laboratoriodeRedesdeComputadoresI;
+	}
+
+	public boolean isLaboratoriodeRedesdeComputadoresII() {
+		return LaboratoriodeRedesdeComputadoresII;
+	}
+
+	public void setLaboratoriodeRedesdeComputadoresII(boolean laboratoriodeRedesdeComputadoresII) {
+		LaboratoriodeRedesdeComputadoresII = laboratoriodeRedesdeComputadoresII;
+	}
+
+	public boolean isLaboratoriodeTelecomunicacoes() {
+		return LaboratoriodeTelecomunicacoes;
+	}
+
+	public void setLaboratoriodeTelecomunicacoes(boolean laboratoriodeTelecomunicacoes) {
+		LaboratoriodeTelecomunicacoes = laboratoriodeTelecomunicacoes;
+	}
+
+	public boolean isSalaAulasMestrado() {
+		return SalaAulasMestrado;
+	}
+
+	public void setSalaAulasMestrado(boolean salaAulasMestrado) {
+		SalaAulasMestrado = salaAulasMestrado;
+	}
+
+	public boolean isSalaAulasMestradoPlus() {
+		return SalaAulasMestradoPlus;
+	}
+
+	public void setSalaAulasMestradoPlus(boolean salaAulasMestradoPlus) {
+		SalaAulasMestradoPlus = salaAulasMestradoPlus;
+	}
+
+	public boolean isSalaNEE() {
+		return SalaNEE;
+	}
+
+	public void setSalaNEE(boolean salaNEE) {
+		SalaNEE = salaNEE;
+	}
+
+	public boolean isSalaProvas() {
+		return SalaProvas;
+	}
+
+	public void setSalaProvas(boolean salaProvas) {
+		SalaProvas = salaProvas;
+	}
+
+	public boolean isSalaReuniao() {
+		return SalaReuniao;
+	}
+
+	public void setSalaReuniao(boolean salaReuniao) {
+		SalaReuniao = salaReuniao;
+	}
+
+	public boolean isSaladeArquitectura() {
+		return SaladeArquitectura;
+	}
+
+	public void setSaladeArquitectura(boolean saladeArquitectura) {
+		SaladeArquitectura = saladeArquitectura;
+	}
+
+	public boolean isSaladeAulasnormal() {
+		return SaladeAulasnormal;
+	}
+
+	public void setSaladeAulasnormal(boolean saladeAulasnormal) {
+		SaladeAulasnormal = saladeAulasnormal;
+	}
+
+	public boolean isVideoconferencia() {
+		return videoconferencia;
+	}
+
+	public void setVideoconferencia(boolean videoconferencia) {
+		this.videoconferencia = videoconferencia;
+	}
+
+	public boolean isAtrio() {
+		return Atrio;
+	}
+
+	public void setAtrio(boolean atrio) {
+		Atrio = atrio;
+	}
+
+}
---
+++ Projeto3/src/main/java/ProjetoADS/Projeto3/teste.java
@@ -0,0 +1,88 @@
+package ProjetoADS.Projeto3;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Retrive information from mysql DB!
+ *
+ */
+public class teste {
+
+	private static Statement stmt;
+	private static ResultSet results;
+
+	public static void main(String[] args) {
+
+		String sql_select = "SELECT * FROM projetoads.`ads - caracterizacao das salas`";
+
+		try (Connection conn = DBconnection.createNewDBconnection()) {
+
+			stmt = conn.createStatement();
+			results = stmt.executeQuery(sql_select);
+
+			List<Sala> studentsList = new ArrayList<Sala>();
+
+			while (results.next()) {
+
+				Sala stdObject = new Sala();
+				System.out.println(results.getString("Nome Sala"));
+				stdObject.setEdificio(results.getString("Edifício"));
+				stdObject.setNomeSala(results.getString("Nome Sala"));
+				stdObject.setCapacidadeNormal(results.getInt("Capacidade Normal"));
+				stdObject.setCapacidadeExame(results.getInt("Capacidade Exame"));				
+				stdObject.setN_caracteristicas(results.getInt("Nº características"));	
+				stdObject.setAnfiteatroaulas(results.getString("Anfiteatro aulas").isBlank());
+				stdObject.setApoiotecnicoeventos(results.getString("Apoio técnico eventos").isBlank());
+				stdObject.setArq1(results.getString("Arq 1").isBlank());
+				stdObject.setArq2(results.getString("Arq 2").isBlank());
+				stdObject.setArq3(results.getString("Arq 3").isBlank());
+				stdObject.setArq4(results.getString("Arq 4").isBlank());
+				stdObject.setArq5(results.getString("Arq 5").isBlank());
+				stdObject.setArq6(results.getString("Arq 6").isBlank());
+				stdObject.setArq9(results.getString("Arq 9").isBlank());
+				stdObject.setBYOD(results.getString("BYOD (Bring Your Own Device)").isBlank());
+				stdObject.setFocusGroup(results.getString("Focus Group").isBlank());
+				stdObject.setHorariosalavisivelportalpublico(results.getString("Horário sala visível portal público").isBlank());
+				stdObject.setLaboratoriodeArquitecturadeComputadoresI(results.getString("Laboratório de Arquitectura de Computadores I").isBlank());
+				stdObject.setLaboratoriodeArquitecturadeComputadoresII(results.getString("Laboratório de Arquitectura de Computadores II").isBlank());
+				stdObject.setLaboratoriodeBasesdeEngenharia(results.getString("Laboratório de Bases de Engenharia").isBlank());
+				stdObject.setLaboratoriodeElectronica(results.getString("Laboratório de Electrónica").isBlank());
+				stdObject.setLaboratoriodeInformatica(results.getString("Laboratório de Informática").isBlank());
+				stdObject.setLaboratoriodeJornalismo(results.getString("Laboratório de Jornalismo").isBlank());
+				stdObject.setLaboratoriodeRedesdeComputadoresI(results.getString("Laboratório de Redes de Computadores I").isBlank());
+				stdObject.setLaboratoriodeRedesdeComputadoresII(results.getString("Laboratório de Redes de Computadores II").isBlank());
+				stdObject.setLaboratoriodeTelecomunicacoes(results.getString("Laboratório de Telecomunicações").isBlank());
+				stdObject.setSalaAulasMestrado(results.getString("Sala Aulas Mestrado").isBlank());
+				stdObject.setSalaAulasMestradoPlus(results.getString("Sala Aulas Mestrado Plus").isBlank());
+				stdObject.setSalaNEE(results.getString("Sala NEE").isBlank());
+				stdObject.setSalaProvas(results.getString("Sala Provas").isBlank());
+				stdObject.setSalaReuniao(results.getString("Sala Reunião").isBlank());
+				stdObject.setSaladeArquitectura(results.getString("Sala de Arquitectura").isBlank());
+				stdObject.setSaladeAulasnormal(results.getString("Sala de Aulas normal").isBlank());
+				stdObject.setVideoconferencia(results.getString("videoconferencia").isBlank());
+				stdObject.setAtrio(results.getString("Átrio").isBlank());
+				
+				
+				studentsList.add(stdObject);
+			}
+
+			ObjectMapper mapper = new ObjectMapper();
+			String JSONOutput = mapper.writeValueAsString(studentsList);
+			System.out.println(JSONOutput);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
