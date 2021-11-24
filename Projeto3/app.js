67b5eb6d358ec57c274f630e882b4241bc55d06a
fnpms-iscte <fnpms@iscte-iul.pt>
2021-11-24 20:24:53 +0000

    Setup app.js e bootstrap

---
+++ Projeto3/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const port =  process.env.port || 3000
+
+app.use(express.static('global'));
+
+app.set('view engine', 'ejs');
+app.listen(port)
+
+app.get('/', (req,res) => {
+    res.render('index', { title: ''} );
+});
+
+app.get('/resultados', (req,res) => {
+    console.log(req)
+    res.render('index', { title: '| Resultados'} );
+});
+
+app.use((req, res) => {
+    res.render('404', { title: '| 404 Error'} );
+});
